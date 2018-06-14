4f3c7d8e1bd36efe9641c86fe5caf5a0ae5e081a
istayreali1123 <istayreal1123@163.com>
2018-06-14 23:26:43 +0800

    jedisnode commit

--- session/src/main/java/com/thanos/session/cache/RedisCache.java
+++ session/src/main/java/com/thanos/session/cache/RedisCache.java
@@ -5,12 +5,15 @@ import com.thanos.session.constants.SessionConstants;
 import org.apache.catalina.Session;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.Protocol;
 
 import javax.print.attribute.standard.NumberUp;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.util.Properties;
+import java.io.Serializable;
+import java.util.*;
 import java.util.logging.Logger;
 
 public class RedisCache implements DataCache {
@@ -50,6 +53,19 @@ public class RedisCache implements DataCache {
         }
         //加载配置文件
         Properties properties = loadProperties();
+        //判断配置文件具体参数
+        //是否集群模式
+        Boolean clusterEnabled = Boolean.valueOf(properties.getProperty(RedisConstants.CLUSTER_ENABLED, RedisConstants.DEFAULT_CLUSTER_ENABLED));
+        //host and password
+        String hosts = properties.getProperty(RedisConstants.HOST, Protocol.DEFAULT_HOST.concat(":").concat(String.valueOf(Protocol.DEFAULT_PORT)));
+        String password = properties.getProperty(RedisConstants.PASSWORD);
+        password = (null != password && password.length() > 0) ? password : null;
+        //database and timeout
+        int dataBase = Integer.parseInt(properties.getProperty(RedisConstants.DATABASE, String.valueOf(Protocol.DEFAULT_DATABASE)));
+        int timeout = Integer.parseInt(properties.getProperty(RedisConstants.TIMEOUT, String.valueOf(Protocol.DEFAULT_TIMEOUT)));
+        timeout = timeout < Protocol.DEFAULT_TIMEOUT ? Protocol.DEFAULT_TIMEOUT : timeout;
+        //get jedis nodes
+        Collection<? extends Serializable> redisNodes = getJedisNodes(hosts, clusterEnabled);
 
     }
 
@@ -72,6 +88,38 @@ public class RedisCache implements DataCache {
             log.error("loadProperties exception : ", exception);
         }
         return rel;
+    }
+
+//此类实现了Serilizable的接口，就可继承Serializable接口
+    private Collection<? extends Serializable> getJedisNodes(String hosts, boolean clusterEnabled) {
+        if (null != hosts && hosts.length() > 0) {
+            //List与Set都为Collection，可以通过泛型直接返回
+            //去掉所有空格
+            hosts = hosts.replaceAll("\\s", "");
+            List<String> node = null;
+            Set<HostAndPort> nodes = null;
+            //10.150.55.2:8080,10.150.55.2:8080
+            String[] hostsList = hosts.split(",");
+            for (String host : hostsList) {
+                String[] hostAndPort = host.split(":");
+                if (clusterEnabled) {
+                    nodes = (null == nodes) ? new HashSet<HostAndPort>() : null;
+                    nodes.add(new HostAndPort(hostAndPort[0], Integer.parseInt(hostAndPort[1])));
+                }else {
+                    //非集群模式，只解析一个节点
+                    int port = Integer.parseInt(hostAndPort[1]);
+                    if (hostAndPort[0].length() > 0 && port > 0) {
+                        node = (null == node) ? new ArrayList<String>() : node;
+                        node.add(hostAndPort[0]);
+                        node.add(hostAndPort[1]);
+                        break;
+                    }
+                }
+            }
+            return clusterEnabled ? nodes : node;
+
+        }
+        return null;
     }
 
 }
--- session/src/main/java/com/thanos/session/utils/RedisCacheUtil.java
+++ session/src/main/java/com/thanos/session/utils/RedisCacheUtil.java
@@ -1,4 +1,19 @@
 package com.thanos.session.utils;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
 public class RedisCacheUtil {
+
+    private JedisPool jedisPool;
+
+    private static int NUM_RETRIES = 3;
+
+    private Log log = LogFactory.getLog(RedisCacheUtil.class);
+
+//    public RedisCacheUtil(String host, int port, String password, int database,int timeout, JedisPoolConfig jedisPoolConfig) {
+//        jedisPool = new JedisPool(jedisPoolConfig, host,)
+//    }
 }
